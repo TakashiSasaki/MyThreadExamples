ec03bccc8f66d79027d00b90d5d36c31e4bfdc9a
Takashi SASAKI <takashi316@gmail.com>
2023-01-11 11:39:36 +0900

    Initial commit

---
+++ app/src/main/java/com/example/mythreadexamples/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.mythreadexamples;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.mythreadexamples.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
